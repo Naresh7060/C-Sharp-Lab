7d929ffd2fb7cf526df231c145bd39e31422abd5
Naresh singh <98658694+Naresh7060@users.noreply.github.com>
2023-08-28 17:47:57 +0530

    Add files via upload

---
+++ 5.Reaverse_array_element.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5.reverse_array_element
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = { 5, 2, 8, 1, 9, 3 };
+
+            Console.WriteLine("Original array:");
+            foreach (int num in arr)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("\nArray in reverse order:");
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+            Console.ReadLine(); 
+        }
+    }
+}
